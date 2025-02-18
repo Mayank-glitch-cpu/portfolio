15d7b036ec8fe5a90332970895dea50ee86601c7
mayank-glitch-cpu <vyasmayank963@gmail.com>
2025-02-17 21:51:32 -0700

    resolved deplyment issue

--- components/dashboards.tsx
+++ components/dashboards.tsx
@@ -4,7 +4,7 @@ const Dashboards = () => {
         <div className="container mx-auto px-4">
           <h2 className="text-3xl font-bold mb-8">Dashboards</h2>
           <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
-          <iframe title="capstone_project" width="100%" height="400" src="https://app.powerbi.com/reportEmbed?reportId=28cfe113-229a-461c-a719-cc7ce42fd44d&autoAuth=true&ctid=41f88ecb-ca63-404d-97dd-ab0a169fd138" frameborder="0" allowFullScreen={true} className="rounded-lg"></iframe>            {/* Add more iframes or other content as needed */}
+          <iframe title="capstone_project" width="100%" height="400" src="https://app.powerbi.com/reportEmbed?reportId=28cfe113-229a-461c-a719-cc7ce42fd44d&autoAuth=true&ctid=41f88ecb-ca63-404d-97dd-ab0a169fd138" frameBorder="0" allowFullScreen={true} className="rounded-lg"></iframe>            {/* Add more iframes or other content as needed */}
           </div>
         </div>
       </section>
